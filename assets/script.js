6bb519961bf15cca0465ceeb2a2a93129b33f2ef
Taysa86 <Taysarolu86@gmail.com>
2022-11-25 20:11:48 -0600

    created prompts for questions

--- assets/script.js
+++ assets/script.js
@@ -15,13 +15,13 @@ var questionBank =
       {Q:"question about javaScript",
        A:"answer about javaScript"}
     ]
-//var startGame = Game()
 
 // Game function
 var run = function () {
     console.log ("good")
     for (var i = 0; i < questionBank.length; i++){
-        console.log(questionBank[i].Q)
+        console.log(questionBank[i].Q) 
+        prompt(questionBank[i].Q)
     }
 
 }
