b3b580baf3e0c02605e750235c7887e846e2a099
Taysa86 <Taysarolu86@gmail.com>
2022-11-25 18:52:24 -0600

    added funtion and listener to button

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+// Button to begin the game
+var generateBtn = document.querySelector("#Start")
+
+//Variables
+var test = ""
+
+//Questions
+//var questionBank = 
+var startGame = Game()
+// Game function
+function Game() {
+    console.log ("good")
+}
+
+
+
+
+// Add event listener to start button
+generateBtn.addEventListener("click", startGame)
