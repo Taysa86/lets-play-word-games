96b8eb2772cba9f8bb83c15f71b9450b70c67c37
Taysa86 <Taysarolu86@gmail.com>
2022-11-25 19:54:19 -0600

    added question function

--- assets/script.js
+++ assets/script.js
@@ -5,15 +5,29 @@ var generateBtn = document.querySelector("#Start")
 var test = ""
 
 //Questions
-//var questionBank = 
-var startGame = Game()
+var questionBank = 
+    [{Q:"How do you indicate the start and end of a code block?",
+      A:"{}" },
+
+      {Q:"How do you create an array?",
+       A:"[]"  },
+
+      {Q:"question about javaScript",
+       A:"answer about javaScript"}
+    ]
+//var startGame = Game()
+
 // Game function
-function Game() {
+var run = function () {
     console.log ("good")
+    for (var i = 0; i < questionBank.length; i++){
+        console.log(questionBank[i].Q)
+    }
+
 }
 
 
 
 
 // Add event listener to start button
-generateBtn.addEventListener("click", startGame)
+generateBtn.addEventListener("click", run)
