3ffcd9a3c0fb14960285b12946221cedbc97b671
Taysa86 <Taysarolu86@gmail.com>
2022-11-27 16:57:52 -0600

    built function to reveal questions on button click

--- assets/script.js
+++ assets/script.js
@@ -5,37 +5,73 @@ var generateBtn = document.querySelector("#Start")
 var response = [];
 var seconds = 60;
 var timededuct = 0;
-var current question = 0;
+var currentquestion = 0;
 //Questions
 var questionBank = 
-    [{Q:"How do you indicate the start and end of a code block?",
-      A:"{}" },
-
-      {Q:"How do you create an array?",
-       A:"[]"  },
-
-      {Q:"question about javaScript",
-       A:"answer about javaScript"}
-    ]
+    [{
+      Question:"Test question 1?",
+      Answer:"A" ,
+      options: 
+        {
+        A: "correct",
+        B: "incorrect",
+        C: "incorrect",
+        D: "incorrect",
+        }  
+      },
+      {
+      Question:"Test question 2?",
+      Answer:"B" ,
+      options: 
+        {
+        A: "incorrect",
+        B: "correct",
+        C: "incorrect",
+        D: "incorrect",
+        }  
+      },
+      {
+      Question:"Test question 3?",
+      Answer:"C" ,
+      options: 
+        {
+        A: "incorrect",
+        B: "incorrect",
+        C: "correct",
+        D: "incorrect",
+        }  
+      },
+      {
+      Question:"Test question 4?",
+      Answer:"D" ,
+      options: 
+        {
+        A: "correct",
+        B: "incorrect",
+        C: "incorrect",
+        D: "incorrect",
+        }  
+      },
+     ]
 
 // Game functions
+//funtion makes the question box appear once game starts
 var showQuestion = function (questionIndex) {
-    document.getElementById('questionText') questions[questionIndex].Question
-    document.getElementById('optionA').textContent
-    console.log ("good")
+    document.getElementById('questionText').textContent = questionBank[questionIndex].Question;
+    document.getElementById('optionA').textContent = questionBank[questionIndex].Options.A;
+    document.getElementById('optionB').textContent = questionBank[questionIndex].Options.B;
+    document.getElementById('optionC').textContent = questionBank[questionIndex].Options.C;
+    document.getElementById('optionD').textContent = questionBank[questionIndex].Options.D;
+    console.log(questions[questionIndex]);
+
     
-    //for (var i = 0; i < questionBank.length; i++){
-        console.log(questionBank[i].Q) 
-        prompt(questionBank[i].Q)
-    
-    
-    //console.log ("True")
+
     }
 
-}
+
 
 
 
 
 // Add event listener to start button
-generateBtn.addEventListener("click", run)
+generateBtn.addEventListener("click", showQuestion)
